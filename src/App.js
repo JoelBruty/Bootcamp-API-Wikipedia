ae94b5c8ca734f75ae29b8a55f836b6cea646940
JoelBruty <165796967+JoelBruty@users.noreply.github.com>
2024-05-22 11:54:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [WikipediaData, setWikipediaData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const fetchHandler = async () => {
+    setWikipediaData(null)
+    try{
+      let response = await fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=info&titles=Wii%20U`)
+      if(!response.ok){
+       throw new Error(response.statusText)
+      }
+      let data = await response.json()
+      console.log(data)
+      setWikipediaData(data)
+    } catch(err) {
+      setError("Could not fetch data")
+      console.log(err.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchHandler()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fetch Wikipedia Data</h1>
+      <button onClick={fetchHandler}>Fetch</button>
+      <br/>
+      {WikipediaData ? <div>{WikipediaData.query.pages.map(pages => pages.name)}</div>:<h2>Loading...</h2>}
     </div>
   );
 }
