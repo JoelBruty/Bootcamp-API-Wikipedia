e9ae6bad9443b92a36acc415dda71d924709e690
JoelBruty <165796967+JoelBruty@users.noreply.github.com>
2024-05-22 15:42:34 +0100

    Added notes and error message for the user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 
 import { useEffect, useState } from 'react';
@@ -8,40 +7,41 @@ function App() {
   const [WikipediaContent, setWikipediaContent] = useState(null)
   const [error, setError] = useState(null)
 
-  let titleQuery = "Wii%20U"
+  let titleQuery = "Wii%20U" //This will be the title that is searched for in the query
 
   const fetchHandler = async () => {
-    setWikipediaContent(null)
-    try{
-      let response = await fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts&titles=${titleQuery}`)
-      if(!response.ok){
-       throw new Error(response.statusText)
+    setWikipediaContent(null) //Clear content in state (makes loading text appear again)
+    setError(null)
+    try {
+      let response = await fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts&titles=${titleQuery}`) //API query
+      if (!response.ok) { //If response is not OK
+        throw new Error(response.statusText) //Throw error with status
       }
-      let data = await response.json()
-      console.log(data.query)
+      let data = await response.json() //Set data
+      console.log(data.query) //Log response query
 
       const { pages } = await data.query;
-      setWikipediaContent(Object.keys(pages).map(id => pages[id].extract))
+      setWikipediaContent(Object.keys(pages).map(id => pages[id].extract)) //Gets extract from response query
 
-    } catch(err) {
-      setError("Could not fetch data")
+    } catch (err) { //Catch error
+      setError(`Could not fetch data: ${err.message}`) //Set error message
       console.log(err.message)
     }
   }
 
   useEffect(() => {
     fetchHandler()
-    
+
   }, [])
 
   return (
     <div>
       <h1>Fetch Wikipedia Data</h1>
       <button onClick={fetchHandler}>Fetch</button>
-      <br/>
+      <br />
       {WikipediaContent ? <div>{WikipediaContent.map(content => (
-        <div dangerouslySetInnerHTML={{ __html: content}}></div>
-      ))}</div>:<h2>Loading...</h2>}
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      ))}</div> : error ? (<h2>{error}</h2>):(<h2>Loading...</h2>)}
     </div>
   );
 }
