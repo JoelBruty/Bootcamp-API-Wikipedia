29561fccf0f4aad6f3e822e60b8b3dcc829299a8
JoelBruty <165796967+JoelBruty@users.noreply.github.com>
2024-05-23 16:26:56 +0100

    Add article title, CSS, misc.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [WikipediaData, setWikipediaData] = useState(null)
+
+  const [WikipediaTitle, setWikipediaTitle] = useState(null)
   const [WikipediaContent, setWikipediaContent] = useState(null)
   const [error, setError] = useState(null)
 
-  let titleQuery = "Wii%20U" //This will be the title that is searched for in the query
+  let titleQuery = "Wii U" //This will be the title/article that is searched for in the query
 
   const fetchHandler = async () => {
-    setWikipediaContent(null) //Clear content in state (makes loading text appear again)
+    //Clear content in state (makes loading text appear again and if an error occured stops displaying the message)
+    setWikipediaData(null)
+
+    setWikipediaTitle(null)
+    setWikipediaContent(null)
     setError(null)
+
     try {
       let response = await fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts&titles=${titleQuery}`) //API query
       if (!response.ok) { //If response is not OK
@@ -23,10 +31,14 @@ function App() {
       console.log(data.query) //Log response query
 
       const { pages } = await data.query;
+
+      setWikipediaData(Object.keys(pages).map(id => [pages[id].title, pages[id].extract]))
+
+      setWikipediaTitle(Object.keys(pages).map(id => pages[id].title))
       setWikipediaContent(Object.keys(pages).map(id => pages[id].extract)) //Gets extract from response query
 
     } catch (err) { //Catch error
-      setError(`Could not fetch data: ${err.message}`) //Set error message
+      setError(`Error: ${err.message}`) //Set error message
       console.log(err.message)
     }
   }
@@ -38,11 +50,14 @@ function App() {
 
   return (
     <div>
-      <h1>Fetch Wikipedia Data</h1>
-      <button onClick={fetchHandler}>Fetch</button>
+      <div className="form">
+        <h1>Fetch Wikipedia Data</h1>
+        <button onClick={fetchHandler}>Fetch</button>
+      </div>
       <br />
-      
-      {WikipediaContent ? <DisplayArticleContent WikipediaContent={WikipediaContent}/> : error ? (<h2>{error}</h2>):(<h2>Loading...</h2>)}
+      <div>
+        {WikipediaContent ? <DisplayArticleContent WikipediaData={WikipediaData} WikipediaTitle={WikipediaTitle} WikipediaContent={WikipediaContent} /> : error ? (<div className="error"><h2>{error}</h2></div>) : (<div className="loading"><h2>Loading...</h2></div>)}
+      </div>
     </div>
   );
 }
--- src/Wiki-App-Components/DisplayArticleContent.js
+++ src/Wiki-App-Components/DisplayArticleContent.js
@@ -1,13 +1,36 @@
 function DisplayArticleContent(props) {
-    const WikipediaContent=props.WikipediaContent
+    const Data = props.WikipediaData
+
+    const Title = props.WikipediaTitle
+    const Article = props.WikipediaContent
+
+    console.log(Data)
+
+    console.log(Title)
+    console.log(Article)
 
     return (
         <div>
-            <div>
-                {WikipediaContent.map(content => (
+            <div className="wiki">
+                <h2>{Title}</h2>
+                {Article.map(content => (
                     <div dangerouslySetInnerHTML={{ __html: content }}></div>
                 ))}
             </div>
+
+            {/* {Article ? (
+                <div className="wiki">
+                    <h2>{Title}</h2>
+                    {Article.map(content => (
+                        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+                    ))}
+                </div>
+            ) : (
+                <div className="error">
+                    <h2>{Title}</h2>
+                    <p>Article content is undefined</p>
+                </div>
+            )} */}
         </div>
     )
 }
